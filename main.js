35fd05fb860016297c6393e48267e6081cb475ac
nick <nicholassam6425@gmail.com>
2022-08-03 12:08:11 -0400

    initial

---
+++ main.js
@@ -0,0 +1,14 @@
+var gold = 0;
+var stage = 1;
+var enemyHP = 10;
+var charList = [];
+
+function generateEnemy(){
+    enemyHP = Math.floor(10 * Math.pow(1.1,stage));
+}
+
+
+
+function attack(dmg){
+
+}
